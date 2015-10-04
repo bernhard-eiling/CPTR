7e3c61917a0435592aad6837fe12841d4c2ca475
Bernhard Eiling <bernhard@blinkist.com>
2015-10-04 21:25:50 +0200

    overriden init methods, renamed ViewController

---
+++ explosure/CameraViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CameraViewController.swift
+//  explosure
+//
+//  Created by Bernhard Eiling on 03.10.15.
+//  Copyright © 2015 bernhardeiling. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+    
+    override init(nibName aNibName: String?, bundle aBundle: NSBundle?) {
+        super.init(nibName: aNibName, bundle: aBundle)
+    }
+    
+    required init?(coder aCoder: NSCoder) {
+        super.init(coder: aCoder)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
