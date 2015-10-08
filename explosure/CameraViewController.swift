54ecf423304c2c047e79417f3da5efe0c5421827
Bernhard Eiling <bernhard@blinkist.com>
2015-10-08 23:34:23 +0200

    adding AVCaptureStillImageOutput to session, method refactoring

--- explosure/CameraViewController.swift
+++ explosure/CameraViewController.swift
@@ -16,6 +16,7 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
     var ciContext = CIContext()
     let glContext = EAGLContext(API: .OpenGLES3)
     var ciImage = CIImage()
+    var glView = GLKView()
     
     override init(nibName aNibName: String?, bundle aBundle: NSBundle?) {
         super.init(nibName: aNibName, bundle: aBundle)
@@ -23,7 +24,7 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
     
     required init?(coder aCoder: NSCoder) {
         super.init(coder: aCoder)
-        setupBackCamera()
+        setupCaptureSession()
     }
     
     override func viewDidLoad() {
@@ -31,14 +32,7 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         setupVideoView()
     }
     
-    func setupVideoView() {
-        
-        ciContext = CIContext(EAGLContext: glContext)
-        let glView = GLKView(frame: self.view.bounds, context: glContext)
-        self.view.addSubview(glView);
-    }
-    
-    func setupBackCamera() {
+    func setupCaptureSession() {
         let backCamera = backCameraDevice()
         let captureDeviceInput : AVCaptureDeviceInput
         do {
@@ -49,6 +43,26 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         } catch {
             NSLog("capture device could not be added to session");
         }
+        let videoDataOutput = AVCaptureVideoDataOutput()
+        videoDataOutput.setSampleBufferDelegate(self, queue: dispatch_queue_create("sample_buffer_queue", DISPATCH_QUEUE_SERIAL))
+        if captureSession.canAddOutput(videoDataOutput) {
+            captureSession.addOutput(videoDataOutput)
+        }
+        let stillImageOutput = AVCaptureStillImageOutput()
+        if captureSession.canAddOutput(stillImageOutput) {
+            captureSession.addOutput(stillImageOutput)
+        }
+        captureSession.sessionPreset = AVCaptureSessionPresetPhoto
+        let sessionQueue = dispatch_queue_create("session_queue", DISPATCH_QUEUE_SERIAL)
+        dispatch_async(sessionQueue) { () -> Void in
+//            self.captureSession.startRunning()
+        }
+    }
+    
+    func setupVideoView() {
+        ciContext = CIContext(EAGLContext: glContext)
+        glView = GLKView(frame: self.view.bounds, context: glContext)
+        view.addSubview(glView);
     }
     
     func captureOutput(captureOutput: AVCaptureOutput!, didDropSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
@@ -57,13 +71,9 @@ class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBuff
         if glContext != EAGLContext.currentContext() {
             EAGLContext.setCurrentContext(glContext)
         }
-        
-    }
-    
-    func videoDataOutput() -> AVCaptureVideoDataOutput? {
-        let videoDataOutput = AVCaptureVideoDataOutput()
-        videoDataOutput.setSampleBufferDelegate(self, queue: dispatch_queue_create(nil, DISPATCH_QUEUE_SERIAL))
-        return videoDataOutput;
+        glView.bindDrawable()
+        ciContext.drawImage(ciImage, inRect: ciImage.extent, fromRect: ciImage.extent)
+        glView.display()
     }
     
     func backCameraDevice() -> AVCaptureDevice? {
